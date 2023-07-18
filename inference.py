b8dd375bb61bb70d93e65696f57b9bee6da944cf
SatArw <satwik090202@gmail.com>
2023-07-18 18:37:31 +0900

    update testing script

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 from tqdm import tqdm
 import numpy as np
 import os, PIL, io, cv2, torch
+import time
 
 if torch.cuda.is_available():
     map_location=lambda storage, loc: storage.cuda()
@@ -12,7 +13,8 @@ else:
 
 nano_path = 'yolov8nano.pt'
 s_path = 'yolov8small.pt'
-model = YOLO(s_path) #edit the path here to choose your model
+cur_path = nano_path
+model = YOLO(cur_path) #edit the path here to choose your model
 
 #load ocr model
 from character_recognition import CharacterRecognizer
@@ -50,51 +52,28 @@ images_arr = []  # Array of image paths
 for file_name in os.listdir(test_path):
     images_arr.append(os.path.join(test_path, file_name))
 
-times_det = []
-times_lbl = []
-
-for file_path in (images_arr):
+st = time.time()
+for file_path in tqdm(images_arr[0:100]):
     # Button detection
     with open(file_path, 'rb') as f:
         img_np = np.asarray(PIL.Image.open(io.BytesIO(f.read())))
-    t0 = cv2.getTickCount()
     preds = model.predict(file_path)
-    t1 = cv2.getTickCount()
-    time = (t1-t0)/cv2.getTickFrequency()
-    times_det.append(time)
-    
+
     for pred in preds:
         boxes = pred.boxes.xyxy.tolist()
         scores = pred.boxes.conf.tolist()
         
     button_patches, button_positions, _ = button_candidates(
         boxes, scores, img_np)
-    
-    t0 = cv2.getTickCount()
+
     for button_img in button_patches:
         # get button text and button_score for each of the images in button_patches
         button_text, button_score, _ = recognizer.predict(button_img)
-    t1 = cv2.getTickCount()
-    time = (t1-t0)/cv2.getTickFrequency()
-    times_lbl.append(time)
-    
-times_total = times_lbl + times_det  # Analyzing total times
-arr = np.array(times_total)
-# measures of dispersion
-avg = np.round(np.mean(arr),5)
-min = np.round(np.amin(arr),5)
-max = np.round(np.amax(arr),5)
-range = np.round(np.ptp(arr),5)
-variance = np.round(np.var(arr),5)
-sd = np.round(np.std(arr),5)
 
 
-print("Average = ", avg)
-print("Minimum =", min)
-print("Maximum =", max)
-print("Range =", range)
-print("Variance =", variance)
-print("Standard Deviation =", sd)
+end = time.time()
+time_taken = end-st
+print(f"Time taken for inferring 100 images with {cur_path} is {time_taken}")
     
     
     
